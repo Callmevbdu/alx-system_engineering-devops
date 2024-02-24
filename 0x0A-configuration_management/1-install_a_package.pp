42535b186fc2b176415e3a3f0e3e72049ddd016f
Abdellatif <abdelt.2012@gmail.com>
2024-02-24 08:07:31 +0100

    Task done.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,15 @@
-#!/usr/bin/pup
+# File: 1-install_a_package.pp
 # Puppet manifest to install Flask version 2.1.0 using pip3
 
-package { 'flask':
-  ensure   => '2.1.0'
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
+}
+
 
