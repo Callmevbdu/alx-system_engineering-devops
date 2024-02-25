0b23f4d4980f3477437b7f00f37abd56f8885dfd
Abdellatif <abdelt.2012@gmail.com>
2024-02-25 03:49:42 +0100

    Task done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,17 @@
-file { '/home/callmevbdu/.ssh/config':
-  ensure  => present,
-  content => "\
-Host 34.239.107.230
-  HostName 34.239.107.230
-  User 421391-web-01
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no\n",
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
