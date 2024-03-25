4ac8f2c389a87c443b1761470ad2cbb571b0ded8
Abdellatif <abdelt.2012@gmail.com>
2024-03-25 17:31:51 +0000

    Done.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,15 +13,6 @@ import sys
 REST_API = "https://jsonplaceholder.typicode.com"
 
 
-def export_to_csv(emp_id, emp_name, tasks):
-    filename = f"{emp_id}.csv"
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in tasks:
-            writer.writerow([emp_id, emp_name, str(task.get('completed')), task.get('title')])
-
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
@@ -30,14 +21,19 @@ if __name__ == '__main__':
             reqTask = requests.get('{}/todos'.format(REST_API)).json()
             empName = req.get('name')
             tasks = list(filter(lambda x: x.get('userId') == empId, reqTask))
-            compTasks = list(filter(lambda x: x.get('completed'), tasks))
             print(
-                'Employee {} is done with tasks({}/{}):'.format(
+                'Employee {} is done with tasks({}):'.format(
                     empName,
-                    len(compTasks),
                     len(tasks)
                 )
             )
-            if len(compTasks) > 0:
-                for task in compTasks:
-                    print('\t {}'.format(task.get('title')))
+            if len(tasks) > 0:
+                for task in tasks:
+                    print(f'\t {task.get("title")}')
+
+        fName = f"{empId}.csv"
+        with open(fName, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            for task in tasks:
+                writer.writerow([empId, empName, str(task.get('completed')), task.get('title')])
