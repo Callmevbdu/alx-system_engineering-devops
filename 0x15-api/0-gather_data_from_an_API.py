f2885c30a68dd79316ef7bf7084a1cb8579a3f8e
Abdellatif <abdelt.2012@gmail.com>
2024-03-25 16:36:38 +0000

    Done.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,15 +15,16 @@ REST_API = "https://jsonplaceholder.typicode.com"
 def get_employee_todo_progress(employee_id):
     req = requests.get(f'{REST_API}/users/{employee_id}').json()
     task_req = requests.get(f'{REST_API}/todos').json()
-    emp_name = req.get('name')
+    empName = req.get('name')
     tasks = [task for task in task_req if task.get('userId') == employee_id]
-    completed_tasks = [task for task in tasks if task.get('completed')]
+    compTask = [task for task in tasks if task.get('completed')]
 
     print(
-        f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):'
+        f'Employee {empName} is done with tasks({len(compTask)}/{len(tasks)}):'
     )
-    for task in completed_tasks:
+    for task in compTask:
         print(f'\t{task.get("title")}')
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and re.fullmatch(r'\d+', sys.argv[1]):
