72ac5e55c778fb420031d6f04e6e0f8d508fe4d1
Abdellatif <abdelt.2012@gmail.com>
2024-03-25 17:38:02 +0000

    Done.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,35 +5,23 @@ information about his/her TODO list progress.
 """
 
 import csv
-import re
 import requests
 import sys
 
 
-REST_API = "https://jsonplaceholder.typicode.com"
-
-
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            empId = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, empId)).json()
-            reqTask = requests.get('{}/todos'.format(REST_API)).json()
-            empName = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == empId, reqTask))
-            print(
-                'Employee {} is done with tasks({}):'.format(
-                    empName,
-                    len(tasks)
-                )
-            )
-            if len(tasks) > 0:
-                for task in tasks:
-                    print(f'\t {task.get("title")}')
+    user = sys.argv[1]
+    userURL = 'https://jsonplaceholder.typicode.com/users/' + user
+    req = requests.get(userURL)
 
-        fName = f"{empId}.csv"
-        with open(fName, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-            for task in tasks:
-                writer.writerow([empId, empName, str(task.get('completed')), task.get('title')])
+    username = req.json().get('username')
+    task = userURL + '/todos'
+    req = requests.get(task)
+    tasks = req.json()
+
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            title_task = task.get('title')
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
