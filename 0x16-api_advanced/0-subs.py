3589a0bd607d96778178fba71cb50feccd0fa644
Abdellatif <abdelt.2012@gmail.com>
2024-04-10 00:18:56 +0000

    Done.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ main function """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'request'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:
