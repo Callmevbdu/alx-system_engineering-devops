f3d6f4bb70f3ca5b1c6086c725b9f7782938e68f
Abdellatif <abdelt.2012@gmail.com>
2024-04-19 02:08:57 +0100

    Done.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,9 @@
 # Define an exec resource to increase open files limit
 exec { 'increase_nginx_open_files' :
-  environment => { 'PATH' => '/etc/default/nginx' },
-  command     => 'ulimit -n 15000',
+  environment => ['DIR=/etc/default/nginx',
+                  'OLD=ULIMIT="-n 15"',
+                  'NEW=ULIMIT="-n 15000"'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
   user        => 'nginx',
   unless      => 'ulimit -n | grep -q 15000',
   path        => ['/usr/bin', '/bin'],
