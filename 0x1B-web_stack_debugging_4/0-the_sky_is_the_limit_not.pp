b8cfbff926473be23791cb6bf0e0df28392f07a3
Abdellatif <abdelt.2012@gmail.com>
2024-04-19 02:13:28 +0100

    Done.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,6 @@ exec { 'increase_nginx_open_files' :
                   'OLD=ULIMIT="-n 15"',
                   'NEW=ULIMIT="-n 15000"'],
   command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
-  user        => 'nginx',
   path        => ['/usr/bin', '/bin'],
   returns     => [0, 1]
 }
